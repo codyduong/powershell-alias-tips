d9354df96246fab82bfeea8e96f630537a6bbf49
Cody Duong <duongcody507@gmail.com>
2023-04-05 22:04:25 -0500

    Update publish.ps1

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -80,8 +80,4 @@ $PSBoundParameters.Remove('Publish')
 Remove-Item $Path -Include ** -Recurse -ErrorAction SilentlyContinue
 New-Item -Path $Path -ItemType Directory -Force -ErrorAction SilentlyContinue | Out-Null
 Copy-Item src/** -Destination $Path -Include **
-if ($Publish -ne $true) {
-  Publish-Module -Path $Path -NuGetApiKey $NuGetApiKey -Verbose -WhatIf @PSBoundParameters
-} else {
-  Publish-Module -Path $Path -NuGetApiKey $NuGetApiKey -Verbose @PSBoundParameters
-}
+Publish-Module -Path $Path -NuGetApiKey $NuGetApiKey -Verbose -WhatIf:$(-not $Publish) @PSBoundParameters
