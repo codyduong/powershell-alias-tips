98d1116de29b765480956cfac04a4a792e1564d6
Cody Duong <duongcody507@gmail.com>
2023-12-18 12:03:02 -0600

    Add StringExpandable support

--- alias-tips/Public/Find-Alias.ps1
+++ alias-tips/Public/Find-Alias.ps1
@@ -5,38 +5,82 @@ function Find-Alias {
   )
 
   $tokens = @()
-  [void][System.Management.Automation.Language.Parser]::ParseInput($Line, [ref]$tokens, [ref]$null)
+  $ast = [System.Management.Automation.Language.Parser]::ParseInput($Line, [ref]$tokens, [ref]$null)
 
   $queue = [System.Collections.ArrayList]::new()
-  $aliased = ""
+  $extents = @(0, 0)
+  $offset = 0
+  $aliased = $ast.ToString()
 
   foreach ($token in $tokens) {
     $kind = $token.Kind
-    if ('Generic', 'StringLiteralToken', 'Generic', 'Identifier' -contains $kind) {
-      if ($queue.Count -gt 0) {
-        $queue[-1] = "$($queue[-1]) $($token.Text)"
+    # Write-Host ($kind, "'$($token.Text)'" , $token.Extent.StartOffset, $token.Extent.StartColumnNumber)
+    if ('Generic', 'Identifier', 'HereStringLiteral', 'StringLiteral' -contains $kind) {
+      if ($queue.Count -eq 0) {
+        $queue += $token.Text
+        $extents = @(($token.Extent.StartColumnNumber - 1), $token.Extent.EndOffset)
       }
       else {
-        $queue += $token.Text
+        $queue[-1] = "$($queue[-1]) $($token.Text)"
+        $extents = @($extents[0], $token.Extent.EndOffset)
       }
     }
-    # TODO handle StringExpandableToken
     else {
       # When we finish the current token back-alias it
       if ($queue.Count -gt 0) {
         $alias = Find-AliasCommand $queue[-1]
-        $aliased += if ([string]::IsNullOrEmpty($alias)) { $queue[-1] } else { $alias }
+        if (-not [string]::IsNullOrEmpty($alias)) {
+          $saved = $queue[-1].Length - $alias.Length
+          $newleft = $extents[0] + $offset
+          $newright = $extents[1] + $offset
+          $aliased = "$(if ($newLeft -le 0) {''} else {$aliased.Substring(0, $newLeft)})$alias$(if ($newright -ge $aliased.Length) {''} else {$aliased.Substring($newright)})"
+          $offset -= $saved
+        }
       }
-      # TODO: Whitespace preservation? Might require a custom Tokenizer
-      if ('AtCurly', 'AtParen', 'DollarParen', 'LBracket', 'LCurly', 'LParen' -contains $kind) {
-        $aliased += " $($token.Text)"
-      } elseif ('RBracket', 'RCurly', 'RParen' -contains $kind) {
-        $aliased += "$($token.Text) "
-      } else {
-        $aliased += " $($token.Text) "
-      }
+      
+      # Reset the queue
+      $queue = [System.Collections.ArrayList]::new()
+      $extents = @(0, 0)
 
-      $queue += ""
+      if ('HereStringExpandable', 'StringExpandable' -contains $kind) {
+        $ntokens = $token.NestedTokens
+        if ($ntokens.Length -eq 0) {
+          continue
+        }
+        $nqueue = [System.Collections.ArrayList]::new()
+        $nextents = @(0, 0)
+        foreach ($ntoken in $ntokens) {
+          $nkind = $ntoken.Kind
+          # Write-Host ("`t", $nkind, "'$($ntoken.Text)'" , $ntoken.Extent.StartOffset, $ntoken.Extent.StartColumnNumber, $ntoken.Extent.EndOffset, $ntoken.Extent.EndColumnNumber)
+          if ('Generic', 'Identifier', 'HereStringLiteral', 'StringLiteral' -contains $nkind) {
+            if ($nqueue.Count -eq 0) {
+              $nqueue += $ntoken.Text
+              $nextents = @(($ntoken.Extent.StartColumnNumber - 1), $ntoken.Extent.EndOffset)
+            }
+            else {
+              $nqueue[-1] = "$($nqueue[-1]) $($ntoken.Text)"
+              $nextents = @($nextents[0], $ntoken.Extent.EndOffset)
+            }
+          }
+          else {
+            # When we finish the current token back-alias it
+            if ($nqueue.Count -gt 0) {
+              $alias = Find-AliasCommand $nqueue[-1]
+              if (-not [string]::IsNullOrEmpty($alias)) {
+                $saved = $nqueue[-1].Length - $alias.Length
+                $newleft = $nextents[0] + $offset
+                $newright = $nextents[1] + $offset
+                $aliased = "$(if ($newLeft -le 0) {''} else {$aliased.Substring(0, $newLeft)})$alias$(if ($newright -ge $aliased.Length) {''} else {$aliased.Substring($newright)})"
+                $offset -= $saved
+              }
+            }
+
+            # Reset the queue
+            $nqueue = [System.Collections.ArrayList]::new()
+            $nextents = @(0, 0)
+          }
+        }
+      }
     }
   }
 
