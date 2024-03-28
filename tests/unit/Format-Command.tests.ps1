f5fe7a35ce7dde04a265f8bece057460611110f0
Cody Duong <duongcody507@gmail.com>
2024-03-27 21:01:43 -0500

    v1.0.1

--- tests/unit/Format-Command.tests.ps1
+++ tests/unit/Format-Command.tests.ps1
@@ -4,7 +4,7 @@ InModuleScope 'alias-tips' {
       Format-Command " git  checkout  -b`n`"foo  bar`"  " | Should -Be "git checkout -b `"foo  bar`"" 
     }
 
-    it 'Should not modify '' strings' {
+    it "Should not modify '' strings" {
       Format-Command " git  checkout  -b`n'foo  bar'  " | Should -Be "git checkout -b 'foo  bar'" 
     }
   }
