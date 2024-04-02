2bc7acf66b254e8bff9f171130a274c0250cd781
Cody Duong <duongcody507@gmail.com>
2024-04-02 00:27:31 -0500

    Resolves #22

--- tests/unit/Format-Command.tests.ps1
+++ tests/unit/Format-Command.tests.ps1
@@ -7,5 +7,13 @@ InModuleScope 'alias-tips' {
     it "Should not modify '' strings" {
       Format-Command " git  checkout  -b`n'foo  bar'  " | Should -Be "git checkout -b 'foo  bar'" 
     }
+
+    it 'Trim whitespace between arguments' {
+      Format-Command "git     status" | Should -Be "git status"
+    }
+
+    it 'Trim command' {
+      Format-Command "   git status   " | Should -Be "git status"
+    }
   }
 }
